f9e5ce3f4292144610cd3ec11ffbe7313e734c20
Paul Arden <parden@f6r2s4.clusters.42paris.fr>
2025-05-14 13:38:10 +0200

    absorption diffusion depends on incidence angle

--- src/model/model_light.c
+++ src/model/model_light.c
@@ -6,7 +6,7 @@
 /*   By: hsoysal <hsoysal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 16:29:34 by parden            #+#    #+#             */
-/*   Updated: 2025/05/04 18:00:50 by parden           ###   ########.fr       */
+/*   Updated: 2025/05/14 13:37:28 by parden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,29 +41,29 @@ static void	object_normal_at(t_v3 res, t_model *m, t_impact *imp)
 }
 
 
-t_rgb	color_lit(t_model *m, t_impact *imp);
+t_rgb	color_lit(t_model *m, t_impact *imp, t_v3 normal);
 t_rgb	model_light(t_model *m, t_impact *imp)
 {
 	t_rgb	obj_col;
 	t_rgb	amb_col;
 	t_rgb	lit_col;
+	t_v3	normal;
 
 	obj_col = object_color_at(&(m->obj[imp->obj_idx]), imp->pos);
 	amb_col = color_mult(m->amb, obj_col);
-	lit_col = color_lit(m, imp);
+	object_normal_at(normal, m, imp);
+	lit_col = color_lit(m, imp, normal);
 	lit_col = color_mult(lit_col, obj_col);
 	return (color_add(amb_col, lit_col));
 }
 
-t_rgb	color_lit(t_model *m, t_impact *imp)
+t_rgb	color_lit(t_model *m, t_impact *imp, t_v3 normal)
 {
-	t_v3		n;
 	int			i;
 	t_impact	bounce;
 	t_rgb		res;
 	t_v3		to_lit;
 
-	object_normal_at(n, m, imp);
 	ft_memcpy(bounce.ray.pos, imp->pos, sizeof(t_v3));
 	i = 0;
 	res = 0;
@@ -73,11 +73,11 @@ t_rgb	color_lit(t_model *m, t_impact *imp)
 		vec_sub(to_lit, to_lit, imp->pos);
 		ft_memcpy(bounce.ray.dir, to_lit, sizeof(t_v3));
 		vec_normalize(bounce.ray.dir);
-		if (vec_dot(n, bounce.ray.dir) > 0)
+		if (vec_dot(normal, bounce.ray.dir) > 0)
 		{
 			model_impact(m, &bounce);
 			if (bounce.scale > vec_norm(to_lit))
-				res = color_add(res, m->lit[i].col);
+				res = color_add(res, color_scale(m->lit[i].col, vec_dot(normal, bounce.ray.dir)));
 		}
 		i++;
 	}
