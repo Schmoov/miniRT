bbfc3e4393844b2210f80ec1cb8beba5e94ce81f
hsoysal <hsoysal@student.42.fr>
2025-05-19 20:10:46 +0200

    feat: add echap functionnality
    fix #15

--- src/display/display.c
+++ src/display/display.c
@@ -6,16 +6,26 @@
 /*   By: hsoysal <hsoysal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 03:47:43 by hsoysal           #+#    #+#             */
-/*   Updated: 2025/04/24 17:47:20 by hsoysal          ###   ########.fr       */
+/*   Updated: 2025/05/19 20:01:28 by hsoysal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/miniRT.h"
 
+int	escape_key_close(int keycode, void *param)
+{
+	t_application	*app;
+
+	app = (t_application *)param;
+	if (keycode == 65307)
+		clear_all(app);
+	return (0);
+}
+
 void	display_image(t_rgb *image)
 {
 	t_application(app) = {0};
-	int(x), y = -1;
+	int (x), y = -1;
 	initialize_mlx(&app, image);
 	while (++y < HEIGHT)
 	{
@@ -28,5 +38,6 @@ void	display_image(t_rgb *image)
 	free(image);
 	mlx_put_image_to_window(app.mlx, app.win, app.img, 0, 0);
 	mlx_hook(app.win, 17, 0, (void *)clear_all, &app);
+	mlx_key_hook(app.win, escape_key_close, &app);
 	mlx_loop(app.mlx);
 }
