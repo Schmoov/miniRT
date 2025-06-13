3b2368fd39b5e9e31710652b1cd2dfc81d4cbe34
Paul Arden <parden@f1r12s15.clusters.42paris.fr>
2025-06-13 18:16:12 +0200

    Correct cone normal & Norm everything

--- src/model/impact.c
+++ src/model/impact.c
@@ -6,7 +6,7 @@
 /*   By: parden <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 19:12:04 by parden            #+#    #+#             */
-/*   Updated: 2025/06/13 13:25:38 by parden           ###   ########.fr       */
+/*   Updated: 2025/06/13 18:14:14 by parden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void	impact_color(t_model *m, t_impact *imp)
 {
-	//WILL CHANGE
 	t_obj	*obj;
 
 	obj = &(m->obj[imp->obj_idx]);
@@ -30,9 +29,34 @@ void	impact_color(t_model *m, t_impact *imp)
 		imp->col = obj->dsk.col;
 }
 
+static void	normal_cylinder(t_model *m, t_impact *imp, t_obj *obj)
+{
+	t_v3	comp;
+
+	vec_sub(imp->normal, imp->pos, obj->cyl.pos);
+	ft_memcpy(comp, imp->normal, sizeof(t_v3));
+	vec_scale(comp, obj->cyl.ax, vec_dot(imp->normal, obj->cyl.ax));
+	vec_sub(imp->normal, imp->normal, comp);
+	vec_normalize(imp->normal);
+}
+
+static void	normal_cone(t_model *m, t_impact *imp, t_obj *obj)
+{
+	t_v3	comp;
+	float	h;
+
+	vec_sub(imp->normal, imp->pos, obj->con.pos);
+	h = vec_dot(imp->normal, obj->con.ax);
+	ft_memcpy(comp, imp->normal, sizeof(t_v3));
+	vec_scale(comp, obj->con.ax, h);
+	vec_sub(comp, imp->normal, comp);
+	vec_scale(imp->normal, obj->con.ax, h * powf(tanf(obj->con.ang), 2));
+	vec_sub(imp->normal, comp, imp->normal);
+	vec_normalize(imp->normal);
+}
+
 void	impact_normal(t_model *m, t_impact *imp)
 {
-	//WILL CHANGE
 	t_obj	*obj;
 
 	obj = &(m->obj[imp->obj_idx]);
@@ -42,32 +66,13 @@ void	impact_normal(t_model *m, t_impact *imp)
 		ft_memcpy(imp->normal, obj->dsk.nor, sizeof(t_v3));
 	if (obj->type == SPH)
 	{
-		ft_memcpy(imp->normal, imp->pos, sizeof(t_v3));
-		vec_sub(imp->normal, imp->normal, obj->sph.pos);
+		vec_sub(imp->normal, imp->pos, obj->sph.pos);
 		vec_normalize(imp->normal);
 	}
 	if (obj->type == CYL)
-	{
-		t_v3	comp;
-		ft_memcpy(imp->normal, imp->pos, sizeof(t_v3));
-		vec_sub(imp->normal, imp->normal, obj->cyl.pos);
-		ft_memcpy(comp, imp->normal, sizeof(t_v3));
-		vec_scale(comp, obj->cyl.ax, vec_dot(imp->normal, obj->cyl.ax));
-		vec_sub(imp->normal, imp->normal, comp);
-		vec_normalize(imp->normal);
-	}
-	//caca
+		normal_cylinder(m, imp, obj);
 	if (obj->type == CON)
-	{
-		t_v3	comp;
-		ft_memcpy(imp->normal, imp->pos, sizeof(t_v3));
-		vec_sub(imp->normal, imp->normal, obj->con.pos);
-		ft_memcpy(comp, imp->normal, sizeof(t_v3));
-		vec_scale(comp, obj->con.ax, vec_dot(imp->normal, obj->con.ax));
-		vec_sub(imp->normal, imp->normal, comp);
-		vec_normalize(imp->normal);
-	}
-
+		normal_cone(m, imp, obj);
 	if (vec_dot(imp->normal, imp->ray.dir) > 0)
 		vec_opp(imp->normal);
 }
