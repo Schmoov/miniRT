1059d1dc541bc9c780923ace52e4308bafdd6411
hsoysal <hsoysal@student.42.fr>
2025-06-18 19:13:38 +0200

    fixe unknown element in ambient_light

--- src/parser/ambient_lighting.c
+++ src/parser/ambient_lighting.c
@@ -6,7 +6,7 @@
 /*   By: hsoysal <hsoysal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 17:46:59 by hsoysal           #+#    #+#             */
-/*   Updated: 2025/06/15 15:04:15 by hsoysal          ###   ########.fr       */
+/*   Updated: 2025/06/18 19:09:57 by hsoysal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,9 @@ t_parsing_error	parse_ambient_lighting(char *line, t_ambient_lighting *ambient)
 		return (ERR_INVALID_AMBIENT_RATIO);
 	line = parse_rgb(line, &ambient->color);
 	if (!line || !check_rgb(ambient->color))
+		return (ERR_INVALID_AMBIENT_COLOR);
+	line = skip_whitespace(line);
+	if (!char_is_blank(line))
 		return (ERR_INVALID_AMBIENT_COLOR);
 	cpt++;
 	return (NO_ERROR);
